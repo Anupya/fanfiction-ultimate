2cf3e8cf4f5446f937e06e6bb0f8da31b57d0314
Anupya Pamidimukkala <anupya@hotmail.ca>
2025-03-08 03:38:08 -0500

    Refactoring Download js and remove jquery from OnePage

--- Download.js
+++ Download.js
@@ -1,13 +1,27 @@
+/*
 
-/* using window.print */
+The following script runs upon "Download" button click
 
+*/
 
-storyDiv = document.createElement('div');
-storyDiv.setAttribute('id', 'FullStoryDiv');
+function download() {
+    const chaptersDropdown = document.getElementsByName("chapter");
+    let chapters = 1;
 
-StartRetrieving();
+    if (chaptersDropdown.length != 0) {
+        chapters = chaptersDropdown[0].options.length;
+    }
 
-function fetchPage(callback, url, chapter) {
+    let tmpLocation = self.location.href.substring(self.location.href.indexOf("fanfiction.net/s/") + 17)
+    tmpLocation = tmpLocation.substring(0, tmpLocation.indexOf("/"));
+    targetLocation = `https://www.fanfiction.net/s/${tmpLocation}/t('.'t)`;
+
+    storyChapters = new Array(chapters);
+    completedChapters = 0;
+    fetchChapter(compileChapters, targetLocation.replace("t('.'t)", 1), 0);
+}
+
+function fetchChapter(callback, url, chapter) {
     var xhr = new XMLHttpRequest();
     xhr.onreadystatechange = function (data) {
         if (xhr.readyState == 4) {
@@ -19,63 +33,16 @@ function fetchPage(callback, url, chapter) {
             }
         }
     }
-    // Note that any URL fetched here must be matched by a permission in
-    // the manifest.json file!
     xhr.open('GET', url, true);
     xhr.send();
 };
 
-function StartRetrieving() {
-    var chaptersDropdown = document.getElementsByName("chapter");
-    var chapters = 1;
-
-    if (chaptersDropdown.length != 0) {
-        chapters = chaptersDropdown[0].options.length;
-    }
-
-    var tmpLocation = self.location.href.substring(self.location.href.indexOf("fanfiction.net/s/") + 17);
-    tmpLocation = tmpLocation.substring(0, tmpLocation.indexOf("/"));
-    targetLocation = "https://www.fanfiction.net/s/" + tmpLocation + "/t('.'t)";
-
-    storyChapters = new Array(chapters);
-    completedChapters = 0;
-    fetchPage(ParseStoryContent, targetLocation.replace("t('.'t)", 1), 0);
-}
-
-function FinishStory() {
-    var title = document.title;
-
-    if (title.indexOf("Chapter") != -1) {
-        title = title.substring(0, title.indexOf("Chapter"));
-    }
-
-    /* Dear Cas - 59 chapters */
-    /* var story = "<h1>" + title + " - " + storyChapters.length + " chapters</h1><br />"; */
-    var story = "<br /><br /><br /><h1>" + title + "</h1>";
-    story += "<br /><br />";
-    /* story += "<h2>" + storyChapters.length + " chapters</h2><br />"; */
-
-    /* Chapter 1
-    blah blah blah ...
-    */
-    for (var i = 0; i < storyChapters.length; i++) {
-        story += "<br /><br /><h3>Chapter " + (i + 1) + " </h3><br /><br />";
-        story += storyChapters[i];
-        story += "<br /><hr>";
-    }
-
-    storyDiv.innerHTML += story;
-    storyDiv.innerHTML += "<br /><br /><br /><br /><br /><br /><br /><center>Thank you for using FanfictionUltimate! <br /> \
-    Leave a review on Chrome Web Store if you liked it.</center>";
-    DownloadPDF(title);
-}
-
-function ParseStoryContent(story, chapter) {
+function compileChapters(story, chapter) {
     completedChapters += 1;
 
     if (!story) {
         storyChapters[chapter] = "";
-        fetchPage(ParseStoryContent, targetLocation.replace("t('.'t)", completedChapters + 1), completedChapters);
+        fetchChapter(compileChapters, targetLocation.replace("t('.'t)", completedChapters + 1), completedChapters);
     }
     else {
         var root = document.implementation.createHTMLDocument();
@@ -84,18 +51,46 @@ function ParseStoryContent(story, chapter) {
             .innerHTML;
         root = null;
 
+        // Once all chapters are fetched, compile them on one page
         if (completedChapters >= storyChapters.length) {
             storyDiv.innerHTML = "";
 
-            FinishStory();
+            compileChaptersInOnePage();
         }
         else {
-            fetchPage(ParseStoryContent, targetLocation.replace("t('.'t)", completedChapters + 1), completedChapters);
+            fetchChapter(compileChapters, targetLocation.replace("t('.'t)", completedChapters + 1), completedChapters);
         }
     }
 }
 
-function DownloadPDF(title) {
+function compileChaptersInOnePage() {
+    let title = document.title;
+
+    if (title.indexOf("Chapter") != -1) {
+        title = title.substring(0, title.indexOf("Chapter"));
+    }
+
+    // Story title
+    let story = "<br /><br /><br /><h1>" + title + "</h1>";
+    story += "<br /><br />";
+
+    // Chapter title + contents
+    for (let i = 0; i < storyChapters.length; i++) {
+        story += "<br /><br /><h3>Chapter " + (i + 1) + " </h3><br /><br />";
+        story += storyChapters[i];
+        story += "<br /><hr>";
+    }
+    storyDiv.innerHTML += story;
+
+    // Promo for extension
+    storyDiv.innerHTML += "<br /><br /><br /><br /><br /><br /><br /><center>Thank you for using FanfictionUltimate! <br /> \
+    Leave a review on Chrome Web Store if you liked it.</center>";
+
+    // Download the fic
+    openWindow(title);
+}
+
+function openWindow(title) {
     const windowWithFullFanfiction = window.open('', 'PRINT', 'height=800, width=800');
 
     if (!windowWithFullFanfiction) {
@@ -116,3 +111,11 @@ function DownloadPDF(title) {
 
     return true;
 }
+
+function init() {
+    storyDiv = document.createElement('div');
+    storyDiv.setAttribute('id', 'FullStoryDiv');
+    download();
+}
+
+init();
--- README.md
+++ README.md
@@ -15,6 +15,12 @@ The ultimate Chrome extension for the best Fanfiction.net experience.
  3. Enable **developer mode**
  4. Click **Load unpacked extension...** and load the fanfiction-ultimate folder
  
+ ## Why use `window.print()` instead of something like JSPDF for the download functionality?
+
+ I was originally going to use JSPDF which would automatically download the full fanfiction to `Downloads` folder, like it happens in A03. Unfortunately, JSPDF does not support UTF-8 encoding. Without UTF-8 encoding, half the sentences are missing and there are some weird spacing issues.
+
+ Given this, and that it's a better experience for the reader to be able to decide where to store the downloaded fic, I went with window.print().
+
  # Contributing guidelines
  
  Search through the [issues](https://github.com/Anupya/fanfiction-ultimate/issues) and feel free to make a pull request for any of them :)
